caaeecd5362703c19d0fe1ebf6a62f936ec84796
Manan Arora <arora.manan.k1234@gmail.com>
2025-04-06 21:34:16 +0530

    frontend work

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Routes,Route} from "react-router-dom"
+import Home from './pages/Home'
+function App() {
+  return (
+    <div className="flex min-h-screen w-screen flex-col bg-richblack-900 font-inter">
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
